67182bf45a1a809f0c7842112fb235e30e2e0144
APdu77 <apdu77@yahoo.fr>
2022-10-11 14:02:16 +0200

    tp4

---
+++ src/utils/TestMethodeStatic.java
@@ -0,0 +1,20 @@
+package utils;
+
+public class TestMethodeStatic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String chaine = "12";
+		int a = 86 ;
+		int b = 196 ;
+		
+		int result1 = Integer.valueOf(chaine);
+		int result2 = Integer.max(a,b);
+		
+		System.out.println(result1);
+		System.out.println(result2);
+
+	}
+
+}
